68214d3acd65b2ccacc5102fe4e3ef779864b822
suhailmalikmail <smalik3426@gmail.com>
2026-09-09 17:14:16 +0530

    day9

---
+++ data-coding/day-9.py
@@ -0,0 +1,22 @@
+# a = 1
+a = "1"
+# b = 2
+b = "2"
+
+print( int (a) + int (b))
+
+string = "15"
+number = 7
+
+string_number = int(string)
+
+sum = number + string_number
+
+print("The Sum of both the numbers is:", sum)
+
+# implicit typecasting
+
+C = 1.9
+D = 8
+
+print(C + D)
