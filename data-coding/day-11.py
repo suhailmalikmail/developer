dd2d34dfb37e995860a3486483888fbd60918dd8
suhailmalikmail <smalik3426@gmail.com>
2026-09-15 17:20:29 +0530

    day11

---
+++ data-coding/day-11.py
@@ -0,0 +1,21 @@
+name = "suhail"
+frind = "wasim"
+anotherfrind = 'zuhaib'
+
+apple = '''He said,
+Hi malik
+hey I am good
+"I want to eat an apple "'''
+
+print("hello," + name)
+# print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+# print(name[6]) #Throws an error
+print("lets  use a for loop\n ")
+for character in apple:
+    print(character)
