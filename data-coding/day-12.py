da31af8a9175e96cd2231e9a580c4a6102bf10a3
suhailmalikmail <smalik3426@gmail.com>
2026-09-17 15:59:33 +0530

    day12

---
+++ data-coding/day-12.py
@@ -0,0 +1,10 @@
+fruit = "mango"
+mangolen = len (fruit)
+print(mangolen)
+# print(fruit[0:4])# including 0 but not 4
+# print(fruit[1:4]) # including 1 but not 4
+# print(fruit[:5])
+# print(fruit[0:-3])
+# print ( fruit[: len (fruit)-3])
+print ( fruit[-1: len (fruit)-3])
+print(fruit[-3:-1])
